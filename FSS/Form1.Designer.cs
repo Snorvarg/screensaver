b258bdf93810be8e9adcf107bc889b0871b140b1
Jon Lennryd <jonlennryd@yahoo.se>
2019-04-07 01:16:18 +0200

    first commit

---
+++ FSS/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace FSS
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.daPanel = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.BalloonTipText = "Doubleclick icon in system tray to show again";
+            this.notifyIcon1.BalloonTipTitle = "Fancy ScreenSaver got minimized";
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "Fancy ScreenSaver";
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // daPanel
+            // 
+            this.daPanel.Location = new System.Drawing.Point(12, 40);
+            this.daPanel.Name = "daPanel";
+            this.daPanel.Size = new System.Drawing.Size(417, 287);
+            this.daPanel.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.daPanel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.Panel daPanel;
+    }
+}
+
---
+++ FSS/Form1.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using Gma.System.MouseKeyHook;
+using CefSharp.WinForms;
+
+namespace FSS
+{
+    // Stuff used:
+    // https://www.c-sharpcorner.com/UploadFile/f9f215/how-to-minimize-your-application-to-system-tray-in-C-Sharp/
+    // https://github.com/gmamaladze/globalmousekeyhook
+    // https://docs.microsoft.com/en-us/dotnet/framework/winforms/controls/how-to-make-thread-safe-calls-to-windows-forms-controls
+    // https://github.com/cefsharp/CefSharp
+
+    public enum States
+    {
+        None,
+        Settings,       // När man startar upp första gången samt från system tray.
+        Minimized,
+        Running         // Skärmsläckaren har startat.
+    }
+
+    public partial class Form1 : Form
+    {
+        protected IKeyboardMouseEvents m_GlobalHook;
+        protected DateTime lastChange = DateTime.Now;
+        protected System.Timers.Timer aTimer;
+
+        private delegate void SafeCallDelegate();
+
+        protected States State = States.None;
+
+        protected double startScreenSaverAfterSeconds = 10;
+
+        protected ChromiumWebBrowser browser;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            CefSharp.Cef.EnableHighDPISupport();
+
+            var settings = new CefSettings();
+            CefSharp.Cef.Initialize(settings);
+
+            browser = new ChromiumWebBrowser("www.google.com");
+            daPanel.Controls.Add(browser);
+
+            aTimer = new System.Timers.Timer();
+            aTimer.Elapsed += ATimer_Elapsed;
+            aTimer.Interval = 5000;
+            aTimer.Enabled = true;
+
+            Subscribe();
+
+            State = States.Settings;
+            SetTitle(State.ToString());
+        }
+
+        private void ATimer_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            if (this.WindowState == FormWindowState.Minimized)
+            {
+                DateTime someTimeAgo = DateTime.Now;
+                someTimeAgo = someTimeAgo.Subtract(TimeSpan.FromSeconds(startScreenSaverAfterSeconds));
+                if(lastChange < someTimeAgo)
+                {
+                    ShowScreenSaver();
+                }
+            }
+        }
+
+        // Invoking since we call this from a timer, which is another thread.
+        protected void ShowScreenSaver()
+        {
+            if (this.InvokeRequired)
+            {
+                var d = new SafeCallDelegate(ShowScreenSaver);
+                Invoke(d, new object[] {});
+            }
+            else
+            {
+                Console.WriteLine("Pickaboo!");
+
+                State = States.Running;
+                SetTitle(State.ToString());
+
+                this.Show();
+                this.WindowState = FormWindowState.Normal;
+                notifyIcon1.Visible = false;
+            }
+        }
+
+        protected void SetTitle(string message)
+        {
+            this.Text = "FSS - " + message;
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            Unsubscribe();
+        }
+
+        protected void Subscribe()
+        {
+            // Note: for the application hook, use the Hook.AppEvents() instead
+            m_GlobalHook = Hook.GlobalEvents();
+
+            m_GlobalHook.MouseDownExt += GlobalHookMouseDownExt;
+            m_GlobalHook.KeyPress += GlobalHookKeyPress;
+        }
+
+        public void Unsubscribe()
+        {
+            m_GlobalHook.MouseDownExt -= GlobalHookMouseDownExt;
+            m_GlobalHook.KeyPress -= GlobalHookKeyPress;
+                        //It is recommened to dispose it
+            m_GlobalHook.Dispose();
+        }
+
+        private void GlobalHookKeyPress(object sender, KeyPressEventArgs e)
+        {
+            //Console.WriteLine("KeyPress: \t{0}", e.KeyChar);
+            lastChange = DateTime.Now;
+        }
+
+        private void GlobalHookMouseDownExt(object sender, MouseEventExtArgs e)
+        {
+            //Console.WriteLine("MouseDown: \t{0}; \t System Timestamp: \t{1}", e.Button, e.Timestamp);
+
+            // uncommenting the following line will suppress the middle mouse button click
+            // if (e.Buttons == MouseButtons.Middle) { e.Handled = true; }
+
+            lastChange = DateTime.Now;
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            if (this.WindowState == FormWindowState.Minimized)
+            {
+                State = States.Minimized;
+                SetTitle(State.ToString());
+
+                Hide();
+                notifyIcon1.Visible = true;
+                notifyIcon1.ShowBalloonTip(1000);
+            }
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            State = States.Settings;
+            SetTitle(State.ToString());
+
+            Show();
+            this.WindowState = FormWindowState.Normal;
+            notifyIcon1.Visible = false;
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            var confirmResult = MessageBox.Show("Are you sure? Minimize to system tray to keep screensaver running.", "Click Yes to quit",MessageBoxButtons.YesNo);
+
+            if (confirmResult != DialogResult.Yes)
+            {
+                e.Cancel = true;
+            }
+        }
+    }
+}
